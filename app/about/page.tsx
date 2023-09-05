3f668cbe526dbb1267ec1636b935ecdef0fdea1d
Stuart Lynn <stuart.lynn@gmail.com>
2023-09-05 11:43:31 +0100

    Better sidebar and bug fix for dates

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default async function Home() {
+
+  return (
+    <main className="flex min-h-screen ">
+      <h1>About </h1>
+    </main>
+  )
+}
--- components/Map.tsx
+++ components/Map.tsx
@@ -3,6 +3,8 @@ import Map, { Marker } from "react-map-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 import { useState } from "react"
 import { format } from "date-fns"
+import Link from "next/link"
+import { AboutModal } from "./AboutModal"
 
 const statusColor: Record<string, string> = {
   "Grant Licence": 'green',
@@ -10,43 +12,120 @@ const statusColor: Record<string, string> = {
   "Application Withdrawn": 'red',
 }
 
-const Sidebar: React.FC<{ stl: Record<string, any> }> = ({ stl }) => {
-  if (!stl) {
-    return (
-      <div style={{ position: 'absolute', height: "100vh", boxSizing: 'border-box', padding: "20px", right: "0px", top: "0px", width: "300px", backgroundColor: "white", zIndex: 100 }}>
-        <h1> Select a property to view details</h1>
-      </div>
-    )
-  }
+
+const STLDetails: React.FC<{ stl: Record<string, any> }> = ({ stl }) => {
   return (
-    <div style={{ position: 'absolute', height: "100vh", boxSizing: 'border-box', padding: "20px", right: "0px", top: "0px", width: "400px", backgroundColor: "white", zIndex: 100 }}>
-      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: "20px", alignItems: "center" }}>
+    <div style={{ display: 'grid', overflowY: "auto", gridTemplateColumns: '1fr', gap: "10px", alignItems: "center", flex: 1 }}>
+      <div>
         <p style={{ fontWeight: "bold" }}>Unique Property Reference Number</p>
         <p>{stl["UPRN"]}</p>
+      </div>
 
+      <div>
         <p style={{ fontWeight: "bold" }}>Address</p>
         <p>{stl["Premises address"]}, {stl["Postcode"]}</p>
+      </div>
+
+      <div>
+        <p style={{ fontWeight: "bold" }}>Manager</p>
+        <p>{stl["Manger"] ? stl["Manager"] : "Not Specified"}</p>
+      </div>
+
+      {(stl["Letting Period From"] && stl["Letting Period Until"]) &&
+        <div>
+          <p style={{ fontWeight: "bold" }}>Letting Period</p>
+          <p>{format(stl["Letting Period From"], "yyyy-MM-dd")} to {format(stl["Letting Period Until"], "yyyy-MM-dd")}</p>
+        </div>
+      }
+
+      <div>
         <p style={{ fontWeight: "bold" }}>Status</p>
         <p>{stl["Status"]}</p>
-        <p style={{ fontWeight: "bold" }}>Letting Period</p>
-        <p>{format(stl["Letting Period From"], "yyyy-MM-dd")} to {format(stl["Letting Period Until"], "yyyy-MM-dd")}</p>
-
-        {stl["Decision"] &&
-          <>
-            <p style={{ fontWeight: "bold" }}>Decision</p>
-            <p>{stl["Decision"]}</p>
-          </>
-        }
       </div>
+
+
+      {stl["Decision"] &&
+        <div>
+          <p style={{ fontWeight: "bold" }}>Decision</p>
+          <p>{stl["Decision"]}</p>
+        </div>
+      }
+
+      {stl["Decision Date"] &&
+        <div>
+          <p style={{ fontWeight: "bold" }}>Decision Date</p>
+          <p>{format(stl["Decision Date"], "yyyy-MM-dd")}</p>
+        </div>
+      }
+
+      {stl["Reason for Refusal - if applicatble"] &&
+        <div>
+          <p style={{ fontWeight: "bold" }}>Reason For Refusal</p>
+          <p>{stl["Reason for Refusal - if applicatble"]}</p>
+        </div>
+      }
+
+      <div>
+        <p style={{ fontWeight: "bold" }}>Type of Premises</p>
+        <p>{stl["Type of Premises"]}</p>
+      </div>
+
+      <div>
+        <p style={{ fontWeight: "bold" }}>Short Term Let Type</p>
+        <p>{stl["Short Term Let Type"]}</p>
+      </div>
+
+      <div>
+        <p style={{ fontWeight: "bold" }}>Maximum Occupancy</p>
+        <p>{stl["Maximum Occupancy"]}</p>
+      </div>
+
+      <div>
+        <p style={{ fontWeight: "bold" }}>Number of Bedrooms</p>
+        <p>{stl["Number of Berooms"]}</p>
+      </div>
+
+    </div>
+  )
+}
+
+const Sidebar: React.FC<{ stl: Record<string, any>, stats: Record<string, number> }> = ({ stl, stats }) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "column", position: 'absolute', height: "calc(100vh - 60px)", boxSizing: 'border-box', padding: "20px", right: "0px", top: "60px", width: "300px", backgroundColor: "white", zIndex: 20 }}>
+
+      {stl ?
+        <STLDetails stl={stl} />
+        :
+        <h2>Click a property to see the details of the short term let application there</h2>
+      }
+      <div style={{ borderTop: "1px solid lightgray", boxSizing: 'border-box', padding: "20px" }}>
+        <p>Total Applications: <span style={{ fontWeight: "bold", color: "black" }}>{stats.total}</span></p>
+        <p style={{ color: "#008000" }}>Accepted: <span style={{ fontWeight: "bold", color: "black" }}>{stats.noAccepted}</span></p>
+        <p style={{ color: "#ff0200" }}>Withdrawn / Rejected: <span style={{ fontWeight: "bold", color: "black" }}>{stats.noWithdrawn}</span></p>
+        <p style={{ color: "#ffdd00" }}>Pending: <span style={{ fontWeight: "bold", color: "black" }}>{stats.noPending}</span></p>
+        <p>Missed Geocode: <span style={{ fontWeight: "bold", color: "black" }}>{stats.notGeocoded}</span></p>
+      </div>
+
     </div>
   )
 }
 
 export const STLMap: React.FC<{ stls: Array<Record<string, any>> }> = ({ stls }) => {
   const [selectedSTL, setSelectedSTL] = useState<any | null>(null)
+  const total = stls.length
+  const notGeocoded = stls.filter(s => !s.geocode).length
+  const noAccepted = stls.filter(s => s["Decision"] === "Grant Licence").length
+  const noWithdrawn = stls.filter(s => s["Decision"] === "Application Withdrawn").length
+  const noPending = stls.filter(s => !s["Decision"]).length
+  const [showAboutModal, setShowAboutModal] = useState(false)
+  console.log("Show about modal ", showAboutModal)
 
   return (
-    <div style={{ position: 'relative', width: "100vw", height: "100vh" }}>
+    <div style={{ position: 'absolute', width: "100vw", height: "100vh" }}>
+      <div style={{ width: "100vw", display: "flex", alignItems: 'center', borderBottom: "1px solid lightgray", justifyContent: "space-between", "flexDirection": 'row', height: "60px", padding: '20px', boxSizing: 'border-box', position: "absolute", zIndex: 20, backgroundColor: "white", top: "0px", left: "0px" }}>
+        <h1>Edinburgh Short Term Let Application Explorer</h1>
+        <Link href="/about" target="__blank">About</Link>
+      </div>
       <Map
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
         initialViewState={{
@@ -64,12 +143,10 @@ export const STLMap: React.FC<{ stls: Array<Record<string, any>> }> = ({ stls })
             latitude={stl.geocode.geometry.coordinates[1]}
             color={stl["Decision"] in statusColor ? statusColor[stl["Decision"]] : 'yellow'}
             onClick={() => setSelectedSTL(stl)}
-            scale={(selectedSTL && stl["UPRN"] === selectedSTL['UPRN']) ? 2 : 1}
           />
-
         )}
       </Map>
-      <Sidebar stl={selectedSTL} />
+      <Sidebar stl={selectedSTL} stats={{ noAccepted, noWithdrawn, noPending, total, notGeocoded }} />
     </div>
   )
 
